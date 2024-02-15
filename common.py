17fc09dd2105d030a6a91db43043503958f7499c
Simon Akhter <simonakhter@gmail.com>
2024-02-15 08:44:42 -0500

    updated prints

--- common.py
+++ common.py
@@ -60,7 +60,8 @@ def find_fake(list_of_msgs, list_of_msgs_copy):
     
     for msgs in chosen_msgs:
         chosen_msg = " ".join(msgs)
-        print(f"\nThere is a {chance}% chance {chosen_msg} is a fake message.")
+        print(f"\nThere is a {chance}% chance {chosen_msg} is a fake ", end="")
+        print(f"message out of {len(list_of_msgs_copy)} messages\n")
 
 def pop_from_stack(list_of_msgs, shift_numbers, isFirst=False):
     #remove the last message and shift number from the list
